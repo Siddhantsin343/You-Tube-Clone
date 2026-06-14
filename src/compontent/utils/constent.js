582e4035e6bebfcff88c82f4b9d1b64f9cd18306
Siddhant S <siddhantsin343@gmail.com>
2026-06-14 09:28:17 -0700

    - s added in http

--- src/compontent/utils/constent.js
+++ src/compontent/utils/constent.js
@@ -2,4 +2,4 @@ const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=60&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
-export const YouTube_Search_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YouTube_Search_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
