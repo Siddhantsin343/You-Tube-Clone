e628276259003a08f9ee2545c26fc6270e2c97d9
Siddhant S <siddhantsin343@gmail.com>
2026-06-15 05:03:29 -0700

    added a LIV_CHAT_COUNT + val

--- src/compontent/utils/constent.js
+++ src/compontent/utils/constent.js
@@ -3,3 +3,5 @@ const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 export const YOUTUBE_VIDEOS_API = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=60&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 export const YouTube_Search_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+
+export const LIVE_CHAT_COUNT = 10
